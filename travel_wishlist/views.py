76311d9476099f3848bd188777ff6bebcfb54832
dan <danie@gmail.com>
2026-05-10 11:57:48 -0500

    wishlist app - places, visiting places

---
+++ travel_wishlist/views.py
@@ -0,0 +1,38 @@
+from django.shortcuts import render, redirect
+from .models import Place
+from .forms import NewPlaceForm
+
+
+def place_list(request):
+
+    if request.method == 'POST':
+        # create new place
+        form = NewPlaceForm(request.POST)  # create form from data in request
+        place = form.save()  # create a model object from form
+        if form.is_valid():  # validation against DB constraints
+            place.save()  # saves place to DB
+            return redirect('place_list')  # reloads home page
+        
+    places = Place.objects.filter(visited=False).order_by('name')
+    new_place_form = NewPlaceForm()
+    return render(request, 'travel_wishlist/wishlist.html', {'places': places, 'new_place_form': new_place_form})
+
+
+def places_visited(request):
+    visited = Place.objects.filter(visited=True)
+    return render(request, 'travel_wishlist/visited.html', {'visited': visited})
+
+
+def place_was_visited(request, place_pk):
+    if request.method == 'POST':
+        place = Place.objects.get(pk=place_pk)
+        place.visited = True
+        place.save()
+
+    return redirect('place_list')
+
+
+def about(request):
+    author = 'Daniela'
+    about = 'A website to create a list of places to visit'
+    return render(request, 'travel_wishlist/about.html', {'author': author, 'about': about})
